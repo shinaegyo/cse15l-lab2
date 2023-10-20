f27ebddc87279d42449c12ec3c46c979e58b554c
shinaegyo <137027086+shinaegyo@users.noreply.github.com>
2023-10-19 22:49:21 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -25,7 +25,7 @@ class Handler implements URLHandler {
     }
 }
 
-class Server {
+class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
